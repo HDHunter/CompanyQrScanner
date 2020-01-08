75d01bd9a8bda6d6330bab571f0b337696ca3ccf
HunterZhang <zhangjianqiu@yodo1.com>
2020-01-08 19:48:01 +0800

    create new apkInfos apk.

---
+++ appinfomonitor/src/main/java/com/hunter/appinfomonitor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hunter.appinfomonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity);
+    }
+}
---
+++ appinfomonitor/src/main/java/com/hunter/appinfomonitor/MyApplication.java
@@ -0,0 +1,6 @@
+package com.hunter.appinfomonitor;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+}
